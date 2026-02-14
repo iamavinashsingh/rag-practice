f4ba986bd6e2ce0d47b3db7381697a1762482f34
Avinash Singh <imavinashsingh2@gmail.com>
2026-02-14 18:39:54 +0530

    Env set & LLM connection done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Wroking fine");
+import * as dotenv from 'dotenv';
+dotenv.config();
+import OpenAI from 'openai';
+
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY, 
+});
+
+const response = await client.responses.create({
+  model: 'gpt-5.2',
+  instructions: 'You are a professor that talks like a pirate and behave like a proper backbencher . Who hates this education system.',
+  input: 'Hi , I am Avinash . Tell me a brutal joke about programming',
+});
+
+console.log(response.output_text);
